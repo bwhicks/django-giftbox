d4e2daa182cb2ea63346280393b4414e2120b005
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2017-09-23 23:42:46 -0400

    Add documentation to wrapper functions

--- giftbox/wrappers.py
+++ giftbox/wrappers.py
@@ -1,6 +1,5 @@
 from django.views.static import serve
 from django.http import HttpResponse
-import sys
 try:
     import urlparse as parse
 except ImportError:
@@ -8,7 +7,21 @@ except ImportError:
 
 
 def send_dev_server(request, filename, **kwargs):
+    """
+    Send a file using Django's development server.
 
+    :param request: An instance of :class:`django.http.HttpRequest`
+    :type request: object.
+    :param filename: Name of the file to be served.
+    :type filename: str.
+    :param **kwargs: See notes below.
+    :returns: :class:~`django.http.FileResponse` object.
+
+    :Keyword Arguments:
+        * *doc_root* (``str``) --
+            Valid filepath for Django's development server to 'xsend'
+
+    """
     doc_root = kwargs['doc_root']
 
     response = serve(request, filename, doc_root)
@@ -17,6 +30,18 @@ def send_dev_server(request, filename, **kwargs):
 
 
 def xsendfile(request, filename, **kwargs):
+    """
+    Send a file using an HTTP X-Sendfile or X-Accel-Redirect response.
+    :param request: An instance of :class:`django.http.HttpRequest`
+    :type request: object.
+    :param filename: Name of the file to be served.
+    :type filename: str.
+    :returns: :class:~`django.http.HttpResponse` object.
+
+    :Keyword Arguments:
+        * *sendfile_url* (``str``) --
+            Xsendfile url to pass as part of http response.
+    """
 
     base_url = kwargs['sendfile_url']
     url = parse.urljoin(base_url, filename)
@@ -25,5 +50,6 @@ def xsendfile(request, filename, **kwargs):
     response['X-Sendfile'] = url
     response['X-Accel-Redirect'] = url
     response['Content-Disposition'] = 'attachment; filename=%s' % filename
+    # Delete default 'Content-Type', which indicates HTML
     del response['Content-Type']
     return response
